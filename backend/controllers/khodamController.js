7f0df0dc9704504ac8af0df87cceefa36c0e2123
FerryFL01 <ferryfebrian.lim@gmail.com>
2024-07-18 11:36:29 +0700

    testing hash

--- backend/controllers/khodamController.js
+++ backend/controllers/khodamController.js
@@ -82,7 +82,8 @@ const getHashKhodam = async(req, res) => {
             return res.status(404).json({error: "Khodam not found" })
         }
 
-        return res.status(200).json(khodam);
+        const data = { khodam, multipliedNumber, totalCount }
+        return res.status(200).json(data);
     } catch (error) {
         return res.status(500).json({ error: error.message })
     }
